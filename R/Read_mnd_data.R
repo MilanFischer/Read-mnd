b059d06eb3f216f0379d8bb7d89ab88b07879b1b
MilanFischer <fischer.milan@gmail.com>
2019-09-11 22:07:36 +0200

    initial commit

---
+++ R/Read_mnd_data.R
@@ -0,0 +1,91 @@
+
+ ############################################
+ #                                          #
+ #             mnd files reader             #
+ #        ------------------------          #
+ #  Created on 2019-09-11 by Milan Fischer  #
+ #  email: fischer.mialn@gmail.com          #
+ #                                          #
+ ############################################
+
+rm(list=ls())
+
+library(openair)
+
+# Create a csv file for each mnd file (TRUE or FALSE)
+Write_csv = TRUE
+
+   dir.create('../outputs',showWarnings = FALSE)
+
+if(Write_csv == TRUE){
+   dir.create('../outputs/csv_files',showWarnings = FALSE)
+}
+
+mnd_Files<-list.files('../mnd_files',pattern="\\.mnd$")
+
+for(i in 1:length(mnd_Files))
+{
+
+  Metadata<-readLines(paste('../mnd_files/',mnd_Files[i], sep=''), 500)
+
+  start=which(Metadata == 'Main Data')
+  end=which(Metadata == '')[2]
+
+  # Extract the names of variables
+  Names <- character()
+  for(k in (start+1):(end-1)){
+     Names <- c(Names, strsplit(Metadata[k], split='#')[[1]][2])
+  }
+
+  # Remove the empty space from the names
+  Names <- gsub(' ', '', Names)
+
+  # Extract the variable units
+  Units <- character()
+  for(k in (start+1):(end-1)){
+     Units <- c(Units, strsplit(Metadata[k], split='#')[[1]][3])
+  }
+
+  # Remove the empty space from the names
+  Names <- gsub(' ', '', Names)
+
+  Data <- read.table(paste('../mnd_files/',mnd_Files[i], sep=''), header = FALSE, skip = end, na.strings = 'N/A')
+
+  # Name the variables
+  names(Data) <- Names
+
+  Date <- substr(mnd_Files[i], 1, nchar(mnd_Files[i])-4)
+
+  if(Write_csv == TRUE){
+    write.table(Data, paste('../outputs/csv_files/', Date,'.csv', sep=''), col.names = TRUE, row.names = FALSE, sep = ',')
+  }
+
+  if(i>1){
+    Data_merged <- rbind(Data_merged, Data)
+  }else{
+    Data_merged <- Data
+  }
+
+  print(Date); flush.console()
+}
+
+# Write final output
+Data_merged$time <- gsub(pattern = '.*/', replacement = '', x = Data_merged$time)
+Data_merged$time <- gsub(pattern = 'T', replacement = ' ', x = Data_merged$time)
+Data_merged$time <- gsub(pattern = '+', replacement = 'R', x = Data_merged$time, fixed = TRUE)
+Data_merged$time <- gsub(pattern = 'R.*', replacement = '', x = Data_merged$time)
+
+write.table(Data_merged,'../outputs/Data_mnd_converted.csv', col.names = TRUE, row.names = FALSE, sep = ',')
+
+Data_merged$time <- as.POSIXct(Data_merged$time, origin = '1970-01-01 00:00:00.1', tz = 'GMT', format='%Y-%m-%d %H:%M')
+colnames(Data_merged)[which(colnames(Data_merged) == 'time')] <- 'date'
+
+# The aggragation assumes that the raw data are in 1 minute time step
+
+Min10 <- timeAverage(Data_merged, avg.time = '10 min', data.thresh = 0, statistic = 'mean',
+type = 'default', percentile = NA, start.date = NA, end.date = NA, interval = '1 min', vector.ws = FALSE, fill = FALSE)
+
+# Necessary to allign the time vector in the way that the timestamp indicate the end of the averaging interval
+Min10$date <- Min10$date+9*60
+
+write.table(Min10,'../outputs/Data_mnd_converted_10min.csv',col.names=TRUE,row.names=FALSE,sep=',')
